a356ba1f6ee341eb4dce9c735d7f53f39ff7bad9
damoore <damoore@microsoft.com>
2014-12-27 16:38:05 -0800

    starting the package

---
+++ snippets.Rcheck/00_pkg_src/snippets/R/snippets.R
@@ -0,0 +1,33 @@
+installpackages <- function(packages){
+    packages <- lapply(packages, FUN = function(x) {
+        if (!require(x, character.only = TRUE)) {
+            install.packages(x)
+            library(x, character.only = TRUE)
+        }
+    })
+}
+
+
+loadinfilesinzip <- function(folder=getwd()){
+    currentwd<-getwd()
+    setwd(folder)
+    listofzipfiles <- list.files(pattern=".zip")
+    patternfilestoread <- ".csv"
+    pb<-txtProgressBar(1, length(listofzipfiles), style=3)
+    pbi<-0
+    for (i in listofzipfiles) {
+        pbi<-pbi+1
+        setTxtProgressBar(pb, pbi)
+        zipdir <- tempfile()
+        dir.create(zipdir)
+        unzip(i, exdir = zipdir)
+        b<-strsplit(i, "\\.")
+        b<-b[[1]][1]
+        k<-0
+        for (j in list.files(path = zipdir, pattern=patternfilestoread)){
+            k<-k+1
+            filestr = paste(b,j, sep="_")
+            assign(filestr,read.csv(paste(zipdir,j, sep="\\")), envir = .GlobalEnv)
+        }
+    }
+}
